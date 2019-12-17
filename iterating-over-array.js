e0de740fcc21b2ff3601a6b46d2b1dfb49abbd00
Yani Rodriguez <yani010712@gmail.com>
2019-12-16 22:13:59 -0500

    Iterating over an array

---
+++ iterating-over-array.js
@@ -0,0 +1,10 @@
+let actors = ['Michael Fox', 'Christopher Lloyd', 'Lea Thompson', 'Claudia Wells'];
+
+actors.forEach(actor => console.log(`${actor} is so cool.`))
+
+console.log(
+`Michael Fox is so cool.
+Christopher Lloyd is so cool.
+Lea Thompson is so cool.
+Claudia Wells is so cool.` 
+);
