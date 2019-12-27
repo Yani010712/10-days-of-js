c08303c93bf7940a5c08fe40dddbc08217273147
Yani Rodriguez <yani010712@gmail.com>
2019-12-27 17:51:41 -0500

    Day 7: Regular Expressions III

---
+++ 7-regular-expressions3.js
@@ -0,0 +1,9 @@
+function regexVar(){
+    /*Declare a RegExp object variable named 're'
+    It must capture ALL occurrences of numbers in a string.
+    */
+    const re =/\d+/g;
+    return re;
+}
+
+//const re = new RegExp('\\d+', 'g')
